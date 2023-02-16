5b1d574542709e4ee8a89a5537ed8ee40a904204
Anastasiya <111999983+Nastya-Leto@users.noreply.github.com>
2023-02-16 21:01:32 +0400

    Update Main.java

--- src/main/java/ru/nastya/leto/Main.java
+++ src/main/java/ru/nastya/leto/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Bye");
         System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
+        System.out.println("Bye world!!!!!");
     }
 }
