6c79e14b53e77ac2694133517020e5999145642f
Anastasiya <soidberg91@gmail.com>
2023-02-16 09:27:02 +0400

    Message change

--- src/main/java/ru/nastya/leto/Main.java
+++ src/main/java/ru/nastya/leto/Main.java
@@ -2,6 +2,7 @@ package ru.nastya.leto;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello QA Guru :)");
+        System.out.println("Hello Hello, QA Guru :)");
+        System.out.println("Bye, QA Guru :)");
     }
 }
