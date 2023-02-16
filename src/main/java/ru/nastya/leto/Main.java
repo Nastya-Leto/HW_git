e74047387979fc2c55e6ca497982c4abc232c129
Anastasiya <111999983+Nastya-Leto@users.noreply.github.com>
2023-02-16 20:55:22 +0400

    Update Main.java

--- src/main/java/ru/nastya/leto/Main.java
+++ src/main/java/ru/nastya/leto/Main.java
@@ -2,6 +2,7 @@ package ru.nastya.leto;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Bye");
         System.out.println("Bye world!!!!!");
     }
 }
